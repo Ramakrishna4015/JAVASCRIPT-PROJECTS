27c27f5718e27bb8269fa911b97370df143af388
Ramakrishna4015 <ramakrishnak015@gmail.com>
2024-03-10 21:50:28 +0530

    first commit

---
+++ Review Carousel/app.js
@@ -0,0 +1,13 @@
+const reviews =[
+    {text: "Overall Nice phone.. battery backup is awesome.Camera really good and decent..at this price range it's complete product.. without any second thought go for it",
+    author:"kRK"},
+{
+    text:"Very good, budget friendly phone.Writing review after using 7days.Primary camera quality very good, but in selfie camera could be more good.Otherwise all others features is very good",
+    author:"RAMU"
+},
+{
+    text:"All over 5 star. But one thing BSNL 4G SIM not working in moto g54.I don't know what happened. Same Sim working in Other mobiles",
+    author:"KRISHNA"
+}
+
+];
